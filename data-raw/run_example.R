bb357efd67d7331fdafa9de5b88f564ea8f0acca
Luna Luisa <sanchez.reyes.luna@gmail.com>
2019-01-14 12:40:55 -0500

    more global function defs-a

--- data-raw/run_example.R
+++ data-raw/run_example.R
@@ -1,31 +1,47 @@
-# creating gplatesr package
-# library(usethis)
-setwd("~/")
-usethis::create_package("gplatesr")
-setwd("~/gplatesr")
-usethis::use_git()
-usethis::use_readme_md()
-usethis::use_data_raw()
-# move this file to data-raw
-# move all original function files to R dir
-# write function descriptions. Then:
-roxygen2::roxygenise() # this creates the man folder
-# write down global function definitions inside functions.
-# write some tests:
-usethis::use_test("gplates_reconstruct")
-# write vignette based on files run_example.R and run_plate_boundary_example.R
-install.packages("rmarkdown")
-usethis::use_vignette("gplates_examples")
-# save an image for the readme:
+# library(magrittr)
+# library(rgeos)
+# library(rgdal) # needs gdal > 1.11.0
+# library(ggplot2)
+# library(jsonlite)
+# library(httr)
+# library(ggthemes)
+# install.packages(c("rgeos", "ggthemes"))
+install.packages("mapproj")
 recon_time <- 140
+
 dat <- gplates_reconstruct_coastlines(recon_time)
+length(dat)
+names(dat)
+class(dat)
+mode(dat)
+
 dat2 <- gplates_reconstruct_static_polygons(recon_time)
+length(dat2)
+names(dat2)
+class(dat2)
+mode(dat2)
+
 dat3 <- gplates_plate_polygons(recon_time)
+length(dat3)
+names(dat3)
+class(dat3)
+mode(dat3)
+
+#coords = gplates_reconstruct_point(20,-25,recon_time)
+# getting:
+# Error in file(con, "r") :
+#   cannot open the connection to 'http://gws.gplates.org/reconstruct/reconstruct_points/?points=20,-25&time=140&model=default'
+# In addition: Warning message:
+# In file(con, "r") :
+#   cannot open URL 'https://gws.gplates.org/reconstruct/reconstruct_points/?points=20,-25&time=140&model=default': HTTP status was '400 Bad Request'
+#
 dat_map <- fortify(dat)
 dat2_map <- fortify(dat2)
 dat3_map <- fortify(dat3)
+
 outline <- bbox(dat)
 outline <- data.frame(xmin=-180,xmax=180,ymin=-90,ymax=90)
+
 gg <- ggplot()
 gg <- gg + geom_map(data=dat2_map, map=dat2_map,
                     aes(x=long, y=lat, map_id=id),
@@ -34,7 +50,7 @@ gg <- gg + geom_map(data=dat_map, map=dat_map,
                     aes(x=long, y=lat, map_id=id),
                     color="white", size=0.15, fill="darkkhaki")
 gg <- gg + geom_map(data=dat3_map, map=dat3_map,
-                    ggplot2::aes(x=long, y=lat, map_id=id),
+                    aes(x=long, y=lat, map_id=id),
                     color="red", size=0.15, fill=NA)
 #gg <- gg + geom_point(aes(x=coords[1], y=coords[2]))
 gg <- gg + geom_rect(data=outline,
@@ -46,7 +62,3 @@ gg <- gg + theme_map()
 gg <- gg + ggtitle(sprintf('Time = %0.1fMa', recon_time))
 gg <- gg + theme(panel.border=element_blank())
 gg
-
-png("vignettes/plate-recons-140.png", width = 680, height = 480, units = "px", pointsize = 12)
-print(gg)
-dev.off()
